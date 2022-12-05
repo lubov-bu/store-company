73f2a91518d510227e97e336cf36d368c002fe7f
Lubov <62780599+Lubov-L@users.noreply.github.com>
2022-12-05 15:58:14 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import './styles/app.css'
+import Product from "./components/Product";
+
+function App() {
+    const [products, setProducts] = useState() [
+            {id: 1, title: 'Skirt', price: '2,43$'},
+            {id: 2, title: 'Shirt', price: '2,13$'},
+            {id: 3, title: 'Dress', price: '2,0$'},
+            {id: 4, title: 'Pants', price: '1,3$'}
+        ]
+
+
+  return (
+    <div className="App">
+      <h1>Clothes Store</h1>
+      <div className="goods">
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+          <Product/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Product.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+
+const Product = () => {
+    return (
+        <a href="#" id="product">
+            <div className="product_image"></div>
+            <div className="product_foot">
+                <p className="product_name">Skirt</p>
+                <p className="product_price">2,5$</p>
+            </div>
+        </a>
+    );
+};
+
+export default Product;
