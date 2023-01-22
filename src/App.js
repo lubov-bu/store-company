f52aba39b8a8bafe9de050e6ddd2b91acfa1ae12
Lubov <62780599+Lubov-L@users.noreply.github.com>
2023-01-22 17:37:57 +0300

    edit modal window

--- src/App.js
+++ src/App.js
@@ -4,15 +4,21 @@ import ProductsList from "./components/ProductsList";
 import NavPanel from "./components/UI/Navigation/NavPanel";
 import Footer from "./components/UI/Footer/Footer";
 import LogInModal from "./components/UI/Modals/LogInModal";
-// import LogInForm from "./components/UI/Modals/Form/LogInForm";
 import Button from "./components/UI/Navigation/Buttons/Button";
+import LogInForm from "./components/UI/Modals/Form/LogInForm";
+import cl from "./components/UI/Modals/Form/Form.module.css";
 
 function App() {
     const [modal, setModal] = useState(false);
 
     return (
     <div className="App">
-        <LogInModal visible={modal} setVisisble={setModal}></LogInModal>
+        <LogInModal visible={modal} setVisisble={setModal}>
+            <div className="modalBack" onClick={() => setModal(false)}></div>
+            <LogInForm>
+                <button className={cl.loginExit} onClick={() => setModal(false)}>x</button>
+            </LogInForm>
+        </LogInModal>
         <NavPanel>
             <Button onClick={() => setModal(true)}>Log In</Button>
             <div className="middleLine">|</div>
--- src/components/UI/Modals/LogInModal.jsx
+++ src/components/UI/Modals/LogInModal.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import './LogInModal.css';
-import cl from "./Form/Form.module.css";
 
-const LogInModal = ({visible, setVisible}) => {
+const LogInModal = ({children, visible, setModal}) => {
 
     const rootClasses = ["logInModal"]
 
@@ -12,16 +11,8 @@ const LogInModal = ({visible, setVisible}) => {
 
     return (
         <div>
-            <div className={rootClasses.join(' ')} onClick={() => setVisible(false)}>
-                <div className="logInModalContent">
-                    <div className={cl.logInForm}>
-                        <p>Log in and Go shopping!</p>
-                        <button className={cl.loginExit} onClick={() => setVisible(false)}>x</button>
-                        <input type="text" placeholder="Username" className={cl.loginInput}/>
-                        <input type="text" placeholder="Password" className={cl.loginPassInput}/>
-                        <button className={cl.logInButton}>Log In</button>
-                    </div>
-                </div>
+            <div className={rootClasses.join(' ')} onClick={() => setModal(false)}>
+                {children}
             </div>
         </div>
     );
