215c58f5073b9877c6e885ecfb1ae074290c8b2f
Lubov <62780599+Lubov-L@users.noreply.github.com>
2023-01-24 20:58:27 +0300

    product array

---
+++ src/components/data/dataCloths.js
@@ -0,0 +1,17 @@
+const images = {
+    "top-1" : {
+        "name" : 'top',
+        "image" : '',
+        "price" : '2,96',
+    },
+    "top-2" : {
+        "name": 'top',
+        "image": '',
+        "price": '2,06',
+    },
+    "top-3" : {
+        "name": 'top',
+        "image": '',
+        "price": '1,90',
+    },
+}
