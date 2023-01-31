2ab7e6b0be120da71bba71024e4f8bbccd490ec5
Lubov <62780599+Lubov-L@users.noreply.github.com>
2023-01-31 21:34:34 +0300

    parsing json

--- src/components/UI/Main/Main.jsx
+++ src/components/UI/Main/Main.jsx
@@ -4,28 +4,41 @@ import Product from "./Product";
 
 
 class Main extends React.Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {products: Object, hasError: false, error: null};
+    // }
     constructor(props) {
         super(props);
-        this.state = {products: [], hasError: false, error: null};
+
+        this.state = {
+            data: [],
+        };
     }
 
     componentDidMount() {
         fetch("http://localhost:3000/dataCloths.json")
-            .then((response) => response.json())
-            .then((data) => this.setState({products: data}))
-            // .catch(e => {
-            //     console.log(e);
-            //     this.setState({isFetching: true, error: e });
-            // })
-            .finally(() => {
-                console.log("fewfew");
-            });
+            .then(response => response.json())
+            .then(data => this.setState({ data }));
+            // .then((response) => response.json())
+            // .then((data) => this.setState({products: data}))
+            // // .catch(e => {
+            // //     console.log(e);
+            // //     this.setState({isFetching: true, error: e });
+            // // })
+            // .finally(() => {
+            //     console.log(products);
+            // });
     }
 
     render() {
-        console.log(this.state);
-        const {products} = this.state.products;
-        return (<ProductsList products={products} title="Top"/>)
+        const { data } = this.state;
+        console.log(data[0])
+        // data.forEach(element => console.log(element.price));
+
+        return 3244324234324;
+        // const {products} = this.state.products;
+        // return (<ProductsList products={products} title="Top"/>)
     }
 }
 
