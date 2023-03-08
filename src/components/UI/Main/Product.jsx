b87d6319838d67861a4996e913380d46b5337cdc
Lubov <62780599+Lubov-L@users.noreply.github.com>
2023-03-08 21:42:43 +0300

    show more content on button Show More

--- src/components/UI/Main/Product.jsx
+++ src/components/UI/Main/Product.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Counter from "./Counter";
 import '../../../styles/media.css';
-import { Navigation, Pagination, Mousewheel, Keyboard } from 'swiper';
+import {Navigation, Pagination, Mousewheel, Keyboard} from 'swiper';
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+import {Swiper, SwiperSlide} from 'swiper/react';
 
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -13,7 +13,7 @@ import 'swiper/css/scrollbar';
 class Product extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {data: []};
+        this.state = {data: [], count: 8};
     }
 
     componentDidMount() {
@@ -24,47 +24,64 @@ class Product extends React.Component {
             .then((data) => {
                 data.sort((a, b) => a.name.localeCompare(b.name));
                 this.setState({data});
+            })
+            .catch(error => {
+                console.log(error);
             });
     }
+
+    handleShowMore = () => {
+        this.setState({count: this.state.count + 8});
+    };
 
     render() {
         return (
             <div>
                 <div className="products">
                     <div className="products__list">
-                        {this.state.data.filter((product, index) => index < 8).map((product) =>
-                            <a key={product.id} href="#">
-                                <Swiper className="product__image"
-                                        cssMode={true}
-                                        navigation={true}
-                                        mousewheel={true}
-                                        loop={true}
-                                        modules={[Navigation, Pagination, Mousewheel, Keyboard]}
-                                >
-                                    <SwiperSlide className="product__image__item">
-                                        <img src={product.image} alt="Product"></img>
-                                    </SwiperSlide>
-                                    <SwiperSlide className="product__image__item">
-                                        <img src={product.image2} alt="Product"></img>
-                                    </SwiperSlide>
-                                    <SwiperSlide className="product__image__item">
-                                        <img src={product.image3} alt="Product"></img>
-                                    </SwiperSlide>
-                                </Swiper>
-                                <div className="product__foot">
-                                    <div>
-                                        <p className={"product__name"}>{product.name}</p>
-                                        <p className={"product__price"}>{product.price} $</p>
+                        {this.state.data
+                            .filter((product, index) => index < this.state.count)
+                            .map((product) => (
+                                <a key={product.id} href="#">
+                                    <Swiper className="product__image"
+                                            cssMode={true}
+                                            navigation={true}
+                                            mousewheel={true}
+                                            loop={true}
+                                            modules={[Navigation, Pagination, Mousewheel, Keyboard]}
+                                    >
+                                        <SwiperSlide className="product__image__item">
+                                            <img src={product.image} alt="Product"></img>
+                                        </SwiperSlide>
+                                        <SwiperSlide className="product__image__item">
+                                            <img src={product.image2} alt="Product"></img>
+                                        </SwiperSlide>
+                                        <SwiperSlide className="product__image__item">
+                                            <img src={product.image3} alt="Product"></img>
+                                        </SwiperSlide>
+                                    </Swiper>
+                                    <div className="product__foot">
+                                        <div>
+                                            <p className={"product__name"}>{product.name}</p>
+                                            <p className={"product__price"}>{product.price} $</p>
+                                        </div>
+                                        <div>
+                                            <Counter/>
+                                            <img className="product__cart" src="/images/cart.png" alt="Cart"/>
+                                        </div>
                                     </div>
-                                    <div>
-                                        <Counter/>
-                                        <img className="product__cart" src="/images/cart.png" alt="Cart"/>
-                                    </div>
-                                </div>
-                            </a>
-                        )}
+                                </a>
+                            ))}
                     </div>
-                    <a href="#" className="product__more">show more</a>
+                    {this.state.count < this.state.data.length && (
+                        <a
+                            href="#"
+                            className="product__more"
+                            onClick={this.handleShowMore}
+                        >
+                            show more
+                        </a>
+                    )}
                 </div>
             </div>
         )
