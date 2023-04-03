035194bc58057b67fddaf965e1c88275d031a1db
Lubov <62780599+Lubov-L@users.noreply.github.com>
2023-04-03 20:02:28 +0300

    webpack config edit

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,36 @@
-const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-    entry: { beautyStore: path.resolve(__dirname, "./src/index.js") },
+    entry: {beautyStore: path.resolve(__dirname, "./src/index.js")},
+    mode: "development",
     output: {
-        path: path.resolve(__dirname, "./dist")
+        path: path.resolve(__dirname, "./dist"),
+        clean: true,
     },
     module: {
-
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ["babel-loader"],
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    resolve: {
+        extensions: ["*", ".js", ".jsx", ".css"]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+        }),
+    ],
+    devServer: {
+        port: 3000,
+        hot: true,
     }
 }
